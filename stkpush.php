40ed711532751a25f4c8ba323fe6588ff9dfbf25
shimuli <shimulicedric@gmail.com>
2020-12-14 17:11:26 +0300

    first commit

---
+++ stkpush.php
@@ -0,0 +1,89 @@
+<?php
+$con = mysqli_connect("localhost", "mwlqobar_nyangena", "nyangena@2020", "mwlqobar_NyangenaMpesa");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    die();
+} else {
+    //writeMsg($con, $sql);
+    pay();
+}
+
+function pay()
+{
+    if (isset($_GET['phonenumber']) && $_GET['phonenumber'] != "" && $_GET['amount'] != "") {
+
+        date_default_timezone_set('Africa/Nairobi');
+        $consumerKey = 'AOxaYEJIjDWgkQAAf5QfiDEqcCJd9WME';
+        $consumerSecret = 't6e1T5aKe9sBOlB0';
+        $BusinessShortCode = '174379';
+        $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+        $number = $_GET['phonenumber']; // This is your phone number,
+        $AccountReference = 'Fee254';
+        $TransactionDesc = 'Fee Payment';
+        $Amount = $_GET['amount'];
+
+        $numberTrimed = substr($number, 1);
+        $PartyA = "254{$numberTrimed}";
+
+        # Get the timestamp, format YYYYmmddhms
+        $Timestamp = date('YmdHis'); // 20201210065709    20201210105706
+
+        # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+        $Password = base64_encode($BusinessShortCode . $Passkey . $Timestamp);
+
+        # header for access token
+        $headers = ['Content-Type:application/json; charset=utf8'];
+
+        # M-PESA endpoint urls
+        $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+        $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+        # callback url
+        $CallBackURL = 'http://mbesha.epizy.com/callBack_url.php';
+
+        $curl = curl_init($access_token_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+        $result = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $result = json_decode($result);
+        $access_token = $result->access_token;
+        curl_close($curl);
+
+        # header for stk push
+        $stkheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
+
+        # initiating the transaction
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $initiate_url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
+        $curl_post_data = array(
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => $BusinessShortCode,
+            'Password' => $Password,
+            'Timestamp' => $Timestamp,
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => $Amount,
+            'PartyA' => $PartyA,
+            'PartyB' => $BusinessShortCode,
+            'PhoneNumber' => $PartyA,
+            'CallBackURL' => $CallBackURL,
+            'AccountReference' => $AccountReference,
+            'TransactionDesc' => $TransactionDesc,
+        );
+        $data_string = json_encode($curl_post_data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        $curl_response = curl_exec($curl);
+        print_r($curl_response);
+        echo $curl_response;
+        echo $id;
+        echo $name;
+    } else {
+        echo 'Invalid Request';
+    }
+}
